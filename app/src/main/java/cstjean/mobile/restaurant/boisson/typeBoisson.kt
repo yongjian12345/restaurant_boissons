40196cafb7c067bd996bbaf7fa722b97ab760774
Raphael Ostiguy <2180858@cstjean.qc.ca>
2023-10-05 16:55:58 -0400

    color change

---
+++ app/src/main/java/cstjean/mobile/restaurant/boisson/typeBoisson.kt
@@ -0,0 +1,14 @@
+package cstjean.mobile.restaurant.boisson
+
+/**
+ * Les differants type de boisson
+ *
+ * @author Raphael ostiguy & Yong Jian Qiu
+ */
+enum class typeBoisson {
+    VIN,
+    SPIRITUEUX,
+    APERITIF,
+    BIERE,
+    AUTRE,
+}
